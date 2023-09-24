e6a0659600a263dd63eef9035d7fb33766e05d4a
buvisuriya07 <70657998+buvisuriya07@users.noreply.github.com>
2023-09-25 02:37:47 +0530

    Create data.js

---
+++ src/Data/data.js
@@ -0,0 +1,58 @@
+const productdata = [
+  {
+    productName: "Fancy Product",
+    productImg: "http://placeimg.com/640/480/city",
+    productPrice: "150.00",
+    productRating: 21362,
+    sale: true,
+    productSalePrice: "199.00",
+    id: "1",
+  },
+  {
+    productName: "Special Item",
+    productImg: "http://placeimg.com/640/480/animals",
+    productPrice: "220.00",
+    productRating: 22729,
+    sale: false,
+    productSalePrice: "59.00",
+    id: "2",
+  },
+  {
+    productName: "Popular Item",
+    productImg: "http://placeimg.com/640/480/nightlife",
+    productPrice: "60.00",
+    productRating: 46460,
+    sale: false,
+    productSalePrice: "740.00",
+    id: "3",
+  },
+  {
+    productName: "Sale Item",
+    productImg: "http://placeimg.com/640/480/nature",
+    productPrice: "410.00",
+    productRating: 53485,
+    sale: false,
+    productSalePrice: "537.00",
+    id: "4",
+  },
+  {
+    productName: "Fancy Product",
+    productImg: "http://placeimg.com/640/480/transport",
+    productPrice: "570.00",
+    productRating: 20605,
+    sale: true,
+    productSalePrice: "638.00",
+    id: "5",
+  },
+  {
+    productName: "Special Item",
+    productImg: "http://placeimg.com/640/480/technics",
+    productPrice: "720.00",
+    productRating: 58463,
+    sale: true,
+    productSalePrice: "632.00",
+    id: "6",
+  },
+];
+
+export default productdata;
